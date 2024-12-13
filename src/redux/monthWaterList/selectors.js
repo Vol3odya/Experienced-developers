3fee2b595bcd63135b676b45746b70deac05fe8a
Yana17-ratsa <ratsayana13@gmail.com>
2024-12-13 23:32:03 +0200

    updated monthWaterList and TodayWaterLisr

--- src/redux/monthWaterList/selectors.js
+++ src/redux/monthWaterList/selectors.js
@@ -1,3 +1,13 @@
-
 export const selectLoading = (state) => state.monthWaterList.loading;
 export const selectError = (state) => state.monthWaterList.error;
+
+export const selectItems = (state) => state.monthWaterList.items;
+
+export const selectDate = (state) => state.monthWaterList.date;
+
+export const selectDaylyNorma = (state) => state.monthWaterList.daylyNorma;
+
+export const selectServings = (state) => state.monthWaterList.servings;
+
+export const selectPercentFromDailyNorma = (state) =>
+  state.monthWaterList.percentFromDailyNorma;
--- src/redux/monthWaterList/slice.js
+++ src/redux/monthWaterList/slice.js
@@ -1,76 +1,42 @@
-import { createSlice, createSelector } from "@reduxjs/toolkit";
-import {logout} from "../auth/operations"
+import { createSlice, createSelector } from '@reduxjs/toolkit';
+import { logout } from '../auth/operations';
 
 const initialState = {
-    items: [{
-        date: null,
-        daylyNorm: null,
-        servings: null,
-        planDaylyNorm: null,
-    }],
-    loading: false,
-    error: null
+  items: [
+    {
+      date: null,
+      daylyNorma: null,
+      servings: null,
+      percentFromDailyNorma: null,
+    },
+  ],
+  loading: false,
+  error: null,
 };
 
-const slise = createSlice({
-    name: "todayWaterList",
-    initialState,
-    extraReducers: (builder) => {
-        builder
-            {/*.addCase(fetchContacts.pending, (state) => { 
-                state.loading = true;
-                state.error = false;
-            })
-            .addCase(fetchContacts.fulfilled, (state, action) => {
-                state.items = action.payload;
-                state.loading = false;
-            })
-            .addCase(logOut.fulfilled, () => {
-                return initialState;
-            })
-            .addCase(fetchContacts.rejected, (state) => {
-                state.error = true;
-                state.loading = false;
-             }).addCase(addContact.pending, (state) => { 
-                state.loading = true;
-                state.error = false;
-            })
-            .addCase(addContact.fulfilled, (state, action) => {
-                state.items.push(action.payload);
-                state.loading = false;
-             })
-            .addCase(addContact.rejected, (state) => {
-                state.error = true;
-                state.loading = false;
-            })
-            .addCase(deleteContact.pending, (state) => { 
-                state.loading = true;
-                state.error = false;
-            })
-            .addCase(deleteContact.fulfilled, (state, action) => {
-                state.items = state.items.filter((item) => item.id!==action.payload);
-                state.loading = false;
-             })
-            .addCase(deleteContact.rejected, (state) => {
-                state.error = true;
-                state.loading = false;
-            })*/}
-    }
-
+const slice = createSlice({
+  name: 'monthWaterList',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getMonthWater.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(getMonthWater.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.loading = false;
+      })
+      .addCase(getMonthWater.rejected, (state) => {
+        state.error = true;
+        state.loading = false;
+        state.items = action.payload;
+        state.date = action.payload.date;
+        state.daylyNorma = action.payload.daylyNorma;
+        state.servings = action.payload.servings;
+        state.percentFromDailyNorma = action.payload.percentFromDailyNorma;
+      });
+  },
 });
 
-
-
-export default slise.reducer;
-
-
-
-export const liContacts = (state) => state.contacts.items;
-
-
-export const selectFilteredContacts = createSelector([liContacts, nameFilters], (contacts, filter) => { 
-    if (filter == "") {
-        return contacts;
-    }
-    return contacts.filter(item => item.name.toLowerCase().includes(filter.toLowerCase()));
-});
+export default slice.reducer;
