5111da3d33caf5654c9ec591269e3913be5c9de8
Vol3odya <Vol_odya@ukr.net>
2024-12-16 01:18:16 +0200

    Start9

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -7,10 +7,10 @@ import css from "./Logo.module.css";
 export default function Logo() {
 
 
-  const addres = "/";
+  const addres = "/welcome";
 
   return (
-    <NavLink className={css.logo} to='/' >
+    <NavLink className={css.logo} to={addres} >
       <img className={css.logo_img} src={svg} alt="logo" width="40" height="48" />
       <p className={css.logo_text}>Tracker of water</p>
     </NavLink>
--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -6,8 +6,10 @@ import css from "./UserAuth.module.css";
 
 export default function UserAuth() {
 
+  const addres = "/signin";
+
   return (
-    <NavLink className={css.user} to='/' >
+    <NavLink className={css.user} to={addres} >
       <p className={css.user_text}>Sign in</p>
       <img className={css.user_img} src={svg} alt="User" width="28" height="28"/>
     </NavLink>
