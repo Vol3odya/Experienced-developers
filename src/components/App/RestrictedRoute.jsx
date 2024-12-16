446909ce3275220d55a2c0db4cd15a26ffe907f0
Mykola Naumenko <1712nn2012@gmail.com>
2024-12-17 00:14:41 +0100

    add signin, signup pages
    створив форму верифікації яка через пропс вирішує яку форму рендирити,  потрібно додати нотифікацію через тостік(чи щось інше) про помилки при не правильному signin або signup.
    а також налаштування CSS.

--- src/components/App/RestrictedRoute.jsx
+++ src/components/App/RestrictedRoute.jsx
@@ -2,8 +2,8 @@ import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import { Navigate } from "react-router-dom";
 
-export default function RestrictedRoute({ component, redirectTo }) {
+export default function RestrictedRoute({ component: Component, redirectTo }) {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  return isLoggedIn ? <Navigate to={redirectTo} /> : component;
+  return isLoggedIn ? <Navigate to={redirectTo} /> : <Component/>;
 }
