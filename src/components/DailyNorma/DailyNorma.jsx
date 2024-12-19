3b225e14b3ec304e5d9ac519c14904c9a784a7ed
Centralice <108938330+Centralice@users.noreply.github.com>
2024-12-19 08:16:46 +0100

    working on adaptive

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -1,5 +1,8 @@
-
 import css from "./DailyNorma.module.css";
+
+import bottleImageM from "../../images/svg/bow-m.svg";
+import bottleImageT from "../../images/svg/bow-t.svg";
+import bottleImageD from "../../images/svg/bow-d.svg";
 
 import { useState } from "react";
 import DailyNormaModal from "../DailyNormaModal/DailyNormaModal";
@@ -15,15 +18,32 @@ export default function DailyNorma() {
   };
 
   return (
-    <div className={css.container}>
-      <h2 className={css.header}>My daily norma</h2>
-      <div className={css.bottomContainer}>
-        <p className={css.liters}>1.5 L</p>
-        <p onClick={handleOpenModal} className={css.edit}>
-          Edit
-        </p>
-        {isOpen && <DailyNormaModal closeModal={handleCloseModal} />}
+    <div className={css.wrapper}>
+      <div className={css.container}>
+        <h2 className={css.header}>My daily norma</h2>
+        <div className={css.bottomContainer}>
+          <p className={css.liters}>1.5 L</p>
+          <p onClick={handleOpenModal} className={css.edit}>
+            Edit
+          </p>
+          {isOpen && <DailyNormaModal closeModal={handleCloseModal} />}
+        </div>
       </div>
+      <picture>
+        <source
+          srcSet={bottleImageT}
+          alt="bottle image"
+          media="(min-width: 768px)"
+          className={css.image}
+        />
+        <source
+          srcSet={bottleImageD}
+          alt="bottle image"
+          media="(min-width: 1440px)"
+          className={css.image}
+        />
+        <img src={bottleImageM} alt="bottle image" className={css.image} />
+      </picture>
     </div>
   );
 }
