33fe8c8ee800d47bb95213654d95d9dd4796ca17
Mykola Naumenko <1712nn2012@gmail.com>
2024-12-21 11:46:10 +0100

    test

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useDispatch, useSelector } from "react-redux";
 import { signup, signin } from "../../redux/auth/operations";
 import { selectIsLoading, selectIsEror } from "../../redux/auth/selectors";
-import s from './AuthForm.module.css'
+import s from "./AuthForm.module.css";
 
 const AuthForm = ({ mode }) => {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectIsEror);
 
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
   const validationSchema = Yup.object({
     email: Yup.string().email("Invalid email").required("Required"),
     password: Yup.string().min(6, "Password too short").required("Required"),
     ...(mode === "signup" && {
-        repeatPassword: Yup.string()
+      repeatPassword: Yup.string()
         .oneOf([Yup.ref("password")], "Passwords do not match")
         .required("Required"),
     }),
@@ -35,11 +39,11 @@ const AuthForm = ({ mode }) => {
     setSubmitting(false);
   };
 
-  const formTitle = mode === 'signup' ? 'Sign Up' : 'Sign In';
+  const formTitle = mode === "signup" ? "Sign Up" : "Sign In";
 
   return (
     <div className={s.wrapper}>
-         <h2 className={s.title}>{formTitle}</h2>
+      <h2 className={s.title}>{formTitle}</h2>
       <Formik
         initialValues={{
           email: "",
@@ -53,25 +57,81 @@ const AuthForm = ({ mode }) => {
           <Form className={s.form}>
             <div className={s.item}>
               <label htmlFor="email">Enter your email</label>
-              <Field name="email" type="email" placeholder='E-mail'/>
-              <ErrorMessage name="email" component="span" className={s.attention}/>
+              <Field name="email" type="email" placeholder="E-mail" />
+              <ErrorMessage
+                name="email"
+                component="span"
+                className={s.attention}
+              />
             </div>
-            
+
             <div className={s.item}>
               <label htmlFor="password">Enter your password</label>
-              <Field name="password" type="password" placeholder='Password'/>
-              <ErrorMessage name="password" component="span" className={s.attention}/>
+              <div className={s.passwordWrapper}>
+                <Field
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  name="password"
+                  className={s.input}
+                  autoComplete="current-password"
+                />
+                <svg
+                  className={s.icon}
+                  onClick={togglePasswordVisibility}
+                  aria-hidden="true"
+                >
+                  <use
+                    xlinkHref={`../../images/svg/symbol-defs.svg#${
+                      showPassword ? "icon-eye-open" : "icon-eye-closed"
+                    }`}
+                  />
+                </svg>
+              </div>
+
+              <ErrorMessage
+                name="password"
+                component="span"  
+                className={s.attention}
+              />
             </div>
 
             {mode === "signup" && (
               <div className={s.item}>
                 <label htmlFor="repeatPassword">Repeat password</label>
-                <Field name="repeatPassword" type="password" placeholder='Repeat password'/>
-                <ErrorMessage name="repeatPassword" component="span" className={s.attention}/>
+                <div className={s.passwordWrapper}>
+                  <Field
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Repeat password"
+                    name="repeatPassword"
+                    className={s.input}
+                    autoComplete="new-password"
+                  />
+                  <svg
+                    className={s.icon}
+                    onClick={togglePasswordVisibility}
+                    aria-hidden="true"
+                  >
+                    <use
+                      xlinkHref={`../../images/svg/symbol-defs.svg#${
+                        showPassword ? "icon-eye-open" : "icon-eye-closed"
+                      }`}
+                    />
+                  </svg>
+                </div>
+
+                <ErrorMessage
+                  name="repeatPassword"
+                  component="span"
+                  className={s.attention}
+                />
               </div>
             )}
 
-            <button type="submit" disabled={isSubmitting || isLoading} className={s.submitbutton}>
+            <button
+              type="submit"
+              disabled={isSubmitting || isLoading}
+              className={s.submitbutton}
+            >
               {isLoading
                 ? "Loading..."
                 : mode === "signup"
