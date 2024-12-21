ac118bd932448a41f6110059fad9f749f530d8a4
Vol3odya <161156362+Vol3odya@users.noreply.github.com>
2024-12-21 14:59:59 +0200

    Update operations.js

--- src/redux/user/operations.js
+++ src/redux/user/operations.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = "http://localhost:3000/"; //'https://tracker-of-water-xk7t.onrender.com/';
-
 export const fetchUser = createAsyncThunk(
   'user/fetchUser',
   async (_, thunkAPI) => {
