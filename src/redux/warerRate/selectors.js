488c53c7056873665369072b1d5d280d8e964a51
Yana17-ratsa <ratsayana13@gmail.com>
2024-12-21 11:55:49 +0200

    папку warerRate видалено

--- src/redux/warerRate/selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectLoading = (state) => state.waterRate.loading;
-export const selectError = (state) => state.waterRate.isError;
-export const selectWaterRate = (state) => state.waterRate.data;
