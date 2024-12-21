35a9bb35b181a2e77d5cdc52b18b7d183465f9a4
Vol3odya <161156362+Vol3odya@users.noreply.github.com>
2024-12-21 14:54:52 +0200

    Update operations.js

--- src/redux/monthWaterList/operations.js
+++ src/redux/monthWaterList/operations.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-
 export const getMonthWater = createAsyncThunk(
   '/month/getMonthWater',
   async ({ month, year }, thunkAPI) => {
