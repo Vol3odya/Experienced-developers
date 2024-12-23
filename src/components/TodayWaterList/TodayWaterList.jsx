99e5c8be2b5bfe523858c089148136b8f86d01f1
Vol3odya <Vol_odya@ukr.net>
2024-12-23 15:44:52 +0200

    Start45

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -78,7 +78,7 @@ export default function TodayWaterList() {
       <h2 className={css.header}>Today</h2>
       {/* <div className={css.listWrapper}></div> */}
       <ul className={css.list}>
-        {waterDay.items.map(({ _id, waterVolume, time }) => (
+        {[...waterDay.items].reverse().map(({ _id, waterVolume, time }) => (
           <li key={_id} className={css.item}>
             <div id={_id} className={css.listli}>
              <div className={css.itemInfo}>
