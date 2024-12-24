6930fc7b08b12b7ee420575432f0fae1e242155e
Mykola Naumenko <1712nn2012@gmail.com>
2024-12-24 18:33:56 +0100

    Виправив помилки

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,11 +1,22 @@
 import Header from "../Header/Header";
 import { Suspense } from "react";
+import { useSelector } from "react-redux";
+import { selectIsRefresh } from "../../redux/auth/selectors"; 
+import Loader from "../Loader/Loader"; 
+import css from "./SharedLayout.module.css";
 
 export default function SharedLayout({ children }) {
+  const isRefreshing = useSelector(selectIsRefresh);
+
   return (
-    <div>
+    <div className={css.layout}>
       <Header />
-      <Suspense fallback={null}>{children}</Suspense>
+      {isRefreshing && (
+        <div className={css.loaderOverlay}>
+          <Loader />
+        </div>
+      )}
+      <Suspense fallback={<Loader />}>{children}</Suspense>
     </div>
   );
 }
