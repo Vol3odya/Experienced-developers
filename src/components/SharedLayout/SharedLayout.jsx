4153a21c66f542b9107e2ba2862f0ebddacf3d15
Mykola Naumenko <1712nn2012@gmail.com>
2024-12-26 10:32:19 +0100

    прибрав завантаження лодера під час оновлення сторінки

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,21 +1,21 @@
 import Header from "../Header/Header";
 import { Suspense } from "react";
-import { useSelector } from "react-redux";
-import { selectIsRefresh } from "../../redux/auth/selectors"; 
+// import { useSelector } from "react-redux";
+// import { selectIsRefresh } from "../../redux/auth/selectors"; 
 import Loader from "../Loader/Loader"; 
 import css from "./SharedLayout.module.css";
 
 export default function SharedLayout({ children }) {
-  const isRefreshing = useSelector(selectIsRefresh);
+  // const isRefreshing = useSelector(selectIsRefresh);
 
   return (
     <div className={css.layout}>
       <Header />
-      {isRefreshing && (
+      {/* {isRefreshing && (
         <div className={css.loaderOverlay}>
           <Loader />
         </div>
-      )}
+      )} */}
       <Suspense fallback={<Loader />}>{children}</Suspense>
     </div>
   );
